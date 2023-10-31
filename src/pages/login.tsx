19bfb6a742b9bfeb49031bbc85a3d4f0e61634de
td232009 <td232009acis.fukuoka-u.ac.jp>
2023-11-01 08:50:29 +0900

    レイアウト調整

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,69 +1,42 @@
 //ログインページ
 import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { useState } from 'react';
-import { useRouter } from 'next/router';
-import { maxHeaderSize } from 'http';
-import { url } from 'inspector';
+import { useTheme } from '@mui/material/styles';
 import Image from "next/image";
-import fu_logo from "/public/images/fu_logo.png";
 import ErrorSnackbar from '@/component/mid/error_snack_bar';
-import { useSession } from 'next-auth/react';
-import { signIn } from 'next-auth/react';
+import { useSession, signIn } from 'next-auth/react';
 
-
-const defaultTheme = createTheme();
 
 export default function SignIn() { //サインインページ
-
+  const theme = useTheme();
   const [uid, setuid] = useState(''); // IDの状態を管理するステート
   const [password, setpassword] = useState(''); // パスワードの状態を管理するステート
   const [openErrorSnackbar, setOpenErrorSnackbar] = useState(false);//スナックバーの状態を管理するステート
   const [errorMessage, setErrorMessage] = useState('');//エラーメッセージの状態を管理するステート
-  const router = useRouter();
+  const {data: session} = useSession();
 
-  //   event.preventDefault();
-  //   try {
-  //       const response = await fetch('/api/api_mid',{
-  //         method: 'POST',
-  //         headers: {
-  //             'Content-Type': 'application/json',
-  //           },
-  //         body: JSON.stringify({uid,password})
-  //       });
-      
-  //       const auth_message = await response.json();
-
-  //       if(auth_message.message == '認証成功'){
-  //         router.push('/joboffer');
-  //       }
-  //       else console.log(auth_message.error)
-  //         setErrorMessage(auth_message.error);
-  //         setOpenErrorSnackbar(true);
-  //   } cat
   const handleSubmit = async (event: React.FocusEvent<HTMLFormElement>) => {
     event.preventDefault();
     const userCredentials = { uid:uid,password:password };
-    await signIn('credentials',userCredentials);
+    const response = await signIn('credentials',userCredentials);
+    if(session?.user?.name) {
+      setErrorMessage('エラー')
+      setOpenErrorSnackbar(true);
+    }
   };
 
   const handleCloseErrorSnackbar = () => 
-   {
+  {
     setOpenErrorSnackbar(false);
   };
 
   return (
-    <ThemeProvider theme={defaultTheme}>
+    <>
         <CssBaseline />
         <Box
           sx={{
@@ -72,7 +45,7 @@ export default function SignIn() { //サインインページ
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
-            backgroundImage: 'url("/images/product/background.jpg")',
+            backgroundColor: 'white',
           }}
         >
 
@@ -81,20 +54,22 @@ export default function SignIn() { //サインインページ
                 alignItems: 'center',
                 display: 'flex',
                 flexDirection: 'column',
+                backgroundColor: 'blue',
              }}>
-          <Image
+              ロゴマーク
+          {/* <Image
         src={fu_logo}
         height={200}
         width={350}
         alt="福岡大学"
-      />
+      /> */}
           </Box>
 
           <Box 
           component="form" 
           onSubmit={handleSubmit} 
           noValidate 
-          sx={{ mt: 10,
+          sx={{ mt: 25,
                 alignItems: 'center',
                 width:"90%",
           }}>
@@ -129,8 +104,7 @@ export default function SignIn() { //サインインページ
               fullWidth
               variant="contained"
               size='large'
-              color='primary'
-              sx={{ mt: 8, mb: 1 ,borderRadius: 10}}
+              sx={{ mt: 30, mb: 1 ,borderRadius: 10,backgroundColor: theme.palette.primary.main}}
             >
               サインイン
             </Button>
@@ -141,6 +115,6 @@ export default function SignIn() { //サインインページ
             />
           </Box>
         </Box>
-    </ThemeProvider>
+    </>
   );
 }
