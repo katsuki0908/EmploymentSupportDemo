e5cfae31c88412d70398d3fc704b15cb44ab57ab
dahee <winterdahee@gmail.com>
2023-11-21 00:19:23 +0900

    DELETE可能

--- src/pages/edit_notice.tsx
+++ src/pages/edit_notice.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import Header from "@/component/big/header";
-import AddNewNotice from "@/component/big/add_new_notice"
+import Header from '@/component/big/header';
+import AddNewNotice from '@/component/big/add_new_notice';
 import { useRouter } from 'next/router';
 import { Button } from '@mui/material';
 
@@ -21,10 +23,10 @@ interface Notice {
 const NoticesPage = () => {
   const [notices, setNotices] = useState<Notice[]>([]);
   const [selectedNoticeId, setSelectedNoticeId] = useState<number | null>(null);
-  const [openNotices, setOpenNotices] = React.useState<number[]>([]);
+  const [snackbarOpen, setSnackbarOpen] = React.useState(false);
   const router = useRouter();
 
-  useEffect(() => {
+  useEffect(() => {     //GET
     const fetchNotices = async () => {
       try {
         const response = await fetch("/api/notice", {
@@ -42,14 +44,14 @@ const NoticesPage = () => {
     fetchNotices();
   }, []);
 
-  const handleDelete = async (noticeId: number) => {
+  const handleDelete = async (noticeId: number) => {    //DELETE
     try {
       const response = await fetch(`/api/notice`, {
         method: 'DELETE',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ notice_id: noticeId }), // 객체를 JSON 문자열로 변환하여 body에 포함
+        body: JSON.stringify({ notice_id: noticeId }), // オブジェクトをJSON文字列に変換してbodyに含める
       });
   
       if (response.ok) {
@@ -57,6 +59,10 @@ const NoticesPage = () => {
         setNotices((prevNotices) => prevNotices.filter((notice) => notice.id !== noticeId));
         // Reset selectedNoticeId after successful deletion
         setSelectedNoticeId(null);
+        // Show success message using Snackbar
+        setSnackbarOpen(true);
+        // Reload the page
+        window.location.reload();
       } else {
         console.error('Error while deleting notice');
         // Handle the error or show a user-friendly message
@@ -68,6 +74,10 @@ const NoticesPage = () => {
     }
   };
   
+  const handleCloseSnackbar = () => {
+    setSnackbarOpen(false);
+  };
+
   return (
     <div>
       <Header />
@@ -87,13 +97,13 @@ const NoticesPage = () => {
             <Typography>
               <h2><b>{notice.title}</b></h2>
               <p>{notice.start_date}から{notice.end_date}まで</p>
-              <p>Notice ID: {notice.notice_id}</p> {/* Display the notice ID */}
             </Typography>
           </AccordionSummary>
           <AccordionDetails>
             <Typography>
               {notice.content}
               <Button 
+                variant="outlined"
                 onClick={() => handleDelete(notice.notice_id)}>
                 DELETE
               </Button>
@@ -101,6 +111,14 @@ const NoticesPage = () => {
           </AccordionDetails>
         </Accordion>
       ))}
+
+      {/* Snackbar for success message */}
+      <Snackbar open={snackbarOpen} autoHideDuration={6000} onClose={handleCloseSnackbar}>
+      <MuiAlert elevation={6} variant="filled" severity="success" onClose={handleCloseSnackbar}>
+        Notice deleted successfully!
+      </MuiAlert>
+      </Snackbar>
+
     </div>
   );
 };
