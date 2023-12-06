e4c35da1c0e6d9db4efc2faf17d90b986832ccd8
td232009 <td232009acis.fukuoka-u.ac.jp>
2023-12-06 12:45:49 +0900

    画面遷移ボタンコンポーネント

---
+++ src/component/Atoms/page_change_button.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@mui/material";
+import { PageChangeProps } from "@/types/props";
+
+export default function PageChangeButton(props:PageChangeProps) {
+    return (
+        <Button 
+        href={props.router_path_name} 
+        color="primary" 
+        variant="contained"
+        >
+        ページに移動
+        </Button>
+    );
+  }
