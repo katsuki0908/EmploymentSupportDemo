34645415fedc208bb327bf1cd917f2414bbe31e4
td232009 <td232009acis.fukuoka-u.ac.jp>
2024-01-09 14:18:24 +0900

    初期ページ

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import { useRouter } from "next/router";
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
 
-const HomePage = () => {
-  return (
-    <div>
-      {/* ページのコンテンツ */}
-    </div>
-  );
-};
+export default function Home() {
+    const { data: session } = useSession();
+    const router = useRouter();
 
-export default HomePage;
+    useEffect(() => {
+        if (session?.user?.user_id) {
+            router.push('/top');
+        } else {
+            router.push('/login');
+        }
+    }, [session, router]); 
+    return (
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center', 
+            alignItems: 'center', 
+            height: '100vh' 
+          }}
+        >
+          <CircularProgress />
+        </Box>  
+)}
