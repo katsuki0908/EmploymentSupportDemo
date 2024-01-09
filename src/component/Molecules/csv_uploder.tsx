e8479aa0ebf94182c7b03f2d7558d75234a9fc9f
katsuki0908 <katsukatsupe@gmail.com>
2024-01-10 04:01:12 +0900

    デプロイ用更新

--- src/component/Molecules/csv_uploder.tsx
+++ src/component/Molecules/csv_uploder.tsx
@@ -1,80 +1,80 @@
-// import React, { useState } from "react";
-// import Button from "@mui/material/Button";
-// import { ConfirmDialog } from "../mid/confirm_dialog";
-// import logger from "../../../logger";
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
+import { ConfirmDialog } from "../mid/confirm_dialog";
+import logger from "../../../logger";
 
-// const FileUpload = () => {
-//   const [file, setFile] = useState<File | null>(null); // stateにFileまたはnull型を使用
-//   const [confirm_dialog, setconfirm_dialog] = useState(false);
+const FileUpload = () => {
+  const [file, setFile] = useState<File | null>(null); // stateにFileまたはnull型を使用
+  const [confirm_dialog, setconfirm_dialog] = useState(false);
 
-//   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-//     if (event.target.files) {
-//       setFile(event.target.files[0]); // event.target.filesがundefinedでない場合にのみファイルを設定
-//     }
-//   };
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      setFile(event.target.files[0]); // event.target.filesがundefinedでない場合にのみファイルを設定
+    }
+  };
 
-//   // const handleUpload = async () => {
-//   //   if (file) { // fileがnullでないことを確認
-//   //     const formData = new FormData();
-//   //     formData.append('file', file); // ここではfileはnullではないので追加
-//   //     logger.info(formData)
-//   //     const response = await fetch('/api/upload', {
-//   //       method: 'POST',
-//   //       body: formData,
-//   //     });
+  // const handleUpload = async () => {
+  //   if (file) { // fileがnullでないことを確認
+  //     const formData = new FormData();
+  //     formData.append('file', file); // ここではfileはnullではないので追加
+  //     logger.info(formData)
+  //     const response = await fetch('/api/upload', {
+  //       method: 'POST',
+  //       body: formData,
+  //     });
 
-//   //     // 追加のエラーハンドリングが必要です
-//   //     if (!response.ok) {
-//   //       // エラー処理
-//   //     } else {
-//   //       // 成功時の処理
-//   //       logger.info(formData)
+  //     // 追加のエラーハンドリングが必要です
+  //     if (!response.ok) {
+  //       // エラー処理
+  //     } else {
+  //       // 成功時の処理
+  //       logger.info(formData)
 
-//   //     }
-//   //   }
-//   // };
+  //     }
+  //   }
+  // };
 
-//   const handleUpload = async () => {
-//     if (file) {
-//       const formData = new FormData();
-//       formData.append("file", file);
-//       console.log("送信前のファイル:", file, formData);
-//       const response = await fetch("/api/upload", {
-//         method: "POST",
-//         body: formData,
-//       });
+  const handleUpload = async () => {
+    if (file) {
+      const formData = new FormData();
+      formData.append("file", file);
+      console.log("送信前のファイル:", file, formData);
+      const response = await fetch("/api/upload", {
+        method: "POST",
+        body: formData,
+      });
 
-//       if (response.ok) {
-//         console.log("ファイルが正常に送信されました");
-//       } else {
-//         console.error("ファイルの送信に失敗しました");
-//       }
-//     }
-//   };
+      if (response.ok) {
+        console.log("ファイルが正常に送信されました");
+      } else {
+        console.error("ファイルの送信に失敗しました");
+      }
+    }
+  };
 
-//   const handleConfirmDialogOpen = () => {
-//     setconfirm_dialog(true);
-//   };
+  const handleConfirmDialogOpen = () => {
+    setconfirm_dialog(true);
+  };
 
-//   const handleConfirmDialogClose = () => {
-//     setconfirm_dialog(false);
-//   };
+  const handleConfirmDialogClose = () => {
+    setconfirm_dialog(false);
+  };
 
-//   return (
-//     <div>
-//       <input type="file" onChange={handleFileChange} accept=".xlsx" />
-//       <Button variant="contained" onClick={handleConfirmDialogOpen}>
-//         送信
-//       </Button>
-//       <ConfirmDialog
-//         open={confirm_dialog}
-//         onConfirm={handleUpload}
-//         onCancel={handleConfirmDialogClose}
-//         title="確認"
-//         message="本当に送信しますか？"
-//       />
-//     </div>
-//   );
-// };
+  return (
+    <div>
+      <input type="file" onChange={handleFileChange} accept=".xlsx" />
+      <Button variant="contained" onClick={handleConfirmDialogOpen}>
+        送信
+      </Button>
+      <ConfirmDialog
+        open={confirm_dialog}
+        onConfirm={handleUpload}
+        onCancel={handleConfirmDialogClose}
+        title="確認"
+        message="本当に送信しますか？"
+      />
+    </div>
+  );
+};
 
-// export default FileUpload;
+export default FileUpload;
--- src/pages/config.tsx
+++ src/pages/config.tsx
@@ -1,25 +1,25 @@
-// import CareerPathAddFormDialog from "@/component/Molecules/career_path_add_form_dialog";
-// import CareerActionAddFormDialog from "@/component/Molecules/career_action_add_form";
-// import { Box, Divider } from "@mui/material";
-// import Header from "@/component/big/header";
-// // import { useSession } from "next-auth/react";
-// // import GoToLogInPage from "@/component/Templates/go_to_login_page";
-// import CsvUploader from "@/component/Molecules/csv_uploder";
-// // import DownloadSelectButton from "@/component/Molecules/download_select_button";
+import CareerPathAddFormDialog from "@/component/Molecules/career_path_add_form_dialog";
+import CareerActionAddFormDialog from "@/component/Molecules/career_action_add_form";
+import { Box, Divider } from "@mui/material";
+import Header from "@/component/big/header";
+// import { useSession } from "next-auth/react";
+// import GoToLogInPage from "@/component/Templates/go_to_login_page";
+import CsvUploader from "@/component/Molecules/csv_uploder";
+// import DownloadSelectButton from "@/component/Molecules/download_select_button";
 
-// export default function Config() {
-//   return (
-//     <>
-//       <Box sx={{ height: "100vh", backgroundColor: "secondary.main" }}>
-//         <Header />
-//         <Box sx={{ mt: 10 }}>
-//           <CareerPathAddFormDialog />
-//           <Divider />
-//           <CareerActionAddFormDialog />
-//           <CsvUploader />
-//           {/* <DownloadSelectButton/> */}
-//         </Box>
-//       </Box>
-//     </>
-//   );
-// }
+export default function Config() {
+  return (
+    <>
+      <Box sx={{ height: "100vh", backgroundColor: "secondary.main" }}>
+        <Header />
+        <Box sx={{ mt: 10 }}>
+          <CareerPathAddFormDialog />
+          <Divider />
+          <CareerActionAddFormDialog />
+          <CsvUploader />
+          {/* <DownloadSelectButton/> */}
+        </Box>
+      </Box>
+    </>
+  );
+}
